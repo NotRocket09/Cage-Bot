f5979673d3c0bef68e0a12b910182f69bf5bfc64
Hunter Rabun <hunterrabun@gmail.com>
2022-07-14 20:02:56 -0400

    First Commit

---
+++ CageBot.py
@@ -0,0 +1,153 @@
+from datetime import datetime, timedelta
+from types import NoneType
+import discord
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from random import randrange
+import asyncio
+
+
+def find_tags(tag : BeautifulSoup):
+    return tag.has_attr("data-id") and tag.has_attr("id")
+
+def Get_Slurs():
+    page = urlopen("http://www.rsdb.org/full")
+
+    html = page.read().decode(encoding="iso-8859-1")
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    output = soup.find_all(find_tags)
+    slurlist = []
+    for outputv in output:
+        index = 0
+        slur = Slur()
+        for td in outputv.find_all("td"):
+            out = td.string if type(td.string) != NoneType else ""
+            if index == 0:
+                slur.Slur = out
+            elif index == 1:
+                slur.Race = out
+            elif index == 2:
+                slur.Reason = out
+            #try:
+                #print(out)
+            #except:
+                #break
+            index += 1
+        slurlist.append(slur)
+    return slurlist
+
+class Slur:
+
+    def __init__(self, Slur="", Race="", Reason="") -> None:
+        self.Slur = Slur
+        self.Race = Race
+        self.Reason = Reason
+    
+    def __str__(self) -> str:
+        return self.Slur + '\n' + self.Race + '\n' + self.Reason
+    
+    def copy(self):
+        return Slur(self.Slur, self.Race, self.Reason)
+
+
+def copy_list(list : list) -> list:
+    cp = []
+    for slur in list:
+        cp.append(slur.copy())
+    return cp
+
+class Client(discord.Client):
+
+    def __init__(self):
+        self.slurlist = Get_Slurs()
+        intents = discord.Intents.all()
+        super().__init__(intents=intents)
+        self.Hentais_ID = 500171309641236480
+    
+    async def on_ready(self):
+        print("We have logged in as {0}".format(self.user))
+        await self.Timer()
+
+    async def on_message(self, message):
+        if message.content == "$slur":
+            channel = message.channel
+            slurs = Get_Slurs()
+            rand = randrange(len(slurs))
+            slurlist = self.SetIndexes(slurs.copy())
+            await channel.send(slurlist[rand])
+            await self.SetDurgunName(Get_Slurs()[rand])
+        elif message.content.startswith("$slur_b"):
+            channel = message.channel
+            slurlist = self.GetBlackSlurs()
+            slurlist = self.SetIndexes(slurlist.copy())
+            rand = randrange(len(slurlist))
+            string = slurlist[rand].__str__()
+            await channel.send(string)
+            await self.SetDurgunName(self.GetBlackSlurs()[rand])
+        elif message.content == "$slur_n":
+            channel = message.channel
+            slurlist = self.GetNigSlurs()
+            slurlist = self.SetIndexes(slurlist.copy())
+            rand = randrange(len(slurlist))
+            string = slurlist[rand].__str__()
+            await channel.send(string)
+        elif message.content == "$slur_nn":
+            channel = message.channel
+            slurlist = self.GetNigSlurs()
+            slurlist = self.SetIndexes(slurlist.copy())
+            rand = randrange(len(slurlist))
+            string = slurlist[rand].__str__()
+            await channel.send(string)
+            await self.SetDurgunName(self.GetNigSlurs()[rand])
+    def GetBlackSlurs(self) -> list:
+        black = []
+        for slur in Get_Slurs():
+            if slur.Race == "Blacks":
+                black.append(slur)
+        return black
+    
+    def GetNigSlurs(self) -> list:
+        nig = []
+        for slur in Get_Slurs():
+            if ("nig" in slur.Slur.lower()) and (slur.Slur != "Midnight"):
+                nig.append(slur)
+        return nig
+    
+    def SetIndexes(self, list1):
+        list2 = []
+        for slur in list1:
+            slur.Slur = "Slur: " + slur.Slur
+            slur.Race = "Race: " + slur.Race
+            slur.Reason = "Reason: " + slur.Reason
+            list2.append(slur)
+        return list2
+    
+    async def SetDurgunName(self, slur : Slur):
+        guild = self.get_guild(self.Hentais_ID)
+        user = guild.get_member(278335245915389953)
+        string = slur.Slur
+        await user.edit(nick=string)
+
+    async def Timer(self):
+        slurs = self.GetNigSlurs()
+        while(True):
+            await asyncio.sleep(300)
+            rand = randrange(len(slurs))
+            await self.SetDurgunName(slurs[rand])
+            channel = self.get_channel(797367069070983189)
+            cp = copy_list(slurs)
+            await channel.send(self.SetIndexes(cp)[rand])
+            if len(slurs) > 2:
+                slurs.pop(rand)
+            else:
+                slurs = self.GetNigSlurs()
+
+#TODO Add lock
+
+def main():
+    client = Client()
+    client.run("OTk2NTQ3NjczOTUzOTM5NTU4.GanHrU.4daixHfuO-nc2Ji87CERfgTg2yC4lrlYmALesc")
+if __name__ == "__main__":
+    main()
